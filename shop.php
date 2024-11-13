552956b10a3e46ea56fdf823eb4a8ad9983358d4
ronssae <miguel.malsi@ciit.edu.ph>
2024-11-13 10:52:30 +0800

    Updated Shop Quantity Functionality

--- shop.php
+++ shop.php
@@ -11,19 +11,19 @@ if(!isset($user_id)){
 }
 
 if(isset($_POST['add_to_cart'])){
-
    $product_name = $_POST['product_name'];
    $product_price = $_POST['product_price'];
    $product_image = $_POST['product_image'];
    $product_quantity = $_POST['product_quantity'];
+   $supplier_name = $_POST['supplier_name']; // Get supplier name
 
    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
 
    if(mysqli_num_rows($check_cart_numbers) > 0){
-      $message[] = 'Already added to cart!';
+       $message[] = 'Already added to cart!';
    }else{
-      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
-      $message[] = 'Product added to cart!';
+       mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image, supplier) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image', '$supplier_name')") or die('query failed');
+       $message[] = 'Product added to cart!';
    }
 }
 
@@ -74,10 +74,16 @@ if(isset($_POST['add_to_cart'])){
          <div class="price">₱<?php echo $fetch_products['price']; ?></div>
          <div class="supplier-name"><?php echo $fetch_products['supplier_name']; ?></div>
 
-         <input type="number" min="1" name="product_quantity" value="1" class="qty">
+         <div class="quantity-container">
+            <button type="button" class="qty-btn" onclick="changeQuantity(<?php echo $fetch_products['id']; ?>, -1)">-</button>
+            <input type="number" min="1" name="product_quantity" value="1" class="qty" id="quantity-<?php echo $fetch_products['id']; ?>" readonly>
+            <button type="button" class="qty-btn" onclick="changeQuantity(<?php echo $fetch_products['id']; ?>, 1)">+</button>
+         </div>
+
          <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
          <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
          <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+         <input type="hidden" name="supplier_name" value="<?php echo $fetch_products['supplier_name']; ?>">
          <input type="submit" value="add to cart" name="add_to_cart" class="btn">
       </form>
       <?php
